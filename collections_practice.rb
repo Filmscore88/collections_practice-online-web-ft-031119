509a67605d6b25c12d461e04ba3da7ccd6ae6b6d
Alberto Morales <cmtmorales24@gmail.com>
2019-03-13 15:32:34 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,12 +41,11 @@ def sum_array(array)
 end 
 
 def add_s(array)
-  array.each_with_index.collect {|element,index| 
+  array.each_with_index.collect do |element,index| 
     if index !=1
-    element="#{element}s"}
-      
-    
-  
+    element="#{element}s"
+    end   
+  end 
 end 
 
   
